c0dc5d729c8bb20a358a04f61ea0270f172f6496
Yasmeen Cole <yasmeencole1@gmail.com>
2022-02-25 19:38:06 -0600

    created directory to hold image files

--- space-tourism/src/components/Home.js
+++ space-tourism/src/components/Home.js
@@ -7,7 +7,6 @@ export const Home = () => (
     <>
         <HomeStyles>
             <div className="home-style">
-                {/* <h2>Space Explorers</h2> */}
                 <div className="ff-sans-cond text-light">So you want to want to travel to</div>
                 <div className="fs-700 ff-serif uppercase">Space</div>
                 <div className="ff-sans-normal text-light">Let’s face it, if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</div>
